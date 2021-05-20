e36f50dcd6416cb4d1bce2ed3e2b313dc616c79f
PRANAYGOUR <76206607+PRANAYGOUR@users.noreply.github.com>
2021-05-20 13:00:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var vaccineMuhs1 , vaccineMuhs1Img;
+var vaccineMuhs2 , vaccineMuhs2Img;
+var vaccineMuhs3 , vaccineMuhs3Img;
+
+function preload(){
+  //loading images for oxygen
+  
+  vaccineMuhs1Img = loadImage("vaccineMuhs1.png");
+  vaccineMuhs2Img = loadImage("vaccineMuhs2.png");
+  vaccineMuhs3Img = loadImage("vaccineMuhs3.png");
+
+}
+function setup(){
+createCanvas(3000,3000)
+  // creating sprite image for oxygen available in chennai hospital no.1
+vaccineMuhs1 = createSprite(800, 400,150,150);
+vaccineMuhs1.addImage(vaccineMuhs1Img);
+
+// creating sprite image for oxygen available in chennai hospital no.2
+vaccineMuhs2 = createSprite(800, 1300,150,150);
+vaccineMuhs2.addImage(vaccineMuhs2Img);
+
+
+// creating sprite image for oxygen available in chennai hospital no.3
+vaccineMuhs3 = createSprite(800, 2200,150,150);
+vaccineMuhs3.addImage(vaccineMuhs3Img);
+
+}
+
+function draw(){
+  background("red")
+  drawSprites();
+}
